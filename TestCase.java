6dee294556a182cf635ca3d0a77d18d19de8e3c0
Reformality <67142532+Reformality@users.noreply.github.com>
2020-08-03 14:30:01 -0400

    Add files via upload

---
+++ TestCase.java
@@ -0,0 +1,27 @@
+public class TestCase {
+
+    public static void main(String[] args) {
+
+        /** Testing Class Heritage
+         * Since all classes doesn't inherit from anything, they are all inheriting from Object.
+         */
+        System.out.println("All class are inheriting from Object!");
+
+        /** Testing UserAccount Field
+         *  This section of code will test security of the data type in UserAccount.java
+        */
+        // Construct a new UserAccount
+        UserAccount testUser = new UserAccount("Username","Password");
+
+        //testing Field Modifier
+        StringBuilder testStr = new StringBuilder();
+        //testStr.append(testUser.username);
+        System.out.println("Fields are all private because running testUser.username will produce a compile error!");
+
+        /** Testing Method
+         *  This section of code will test security of the data type in UserAccount.java
+         */
+
+        // continue here...
+    }
+}
